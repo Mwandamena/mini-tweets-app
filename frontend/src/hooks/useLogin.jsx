8295b0253adc8823ce55b13d3bbec7731dad3c17
Mwandamena <mwandamenakelvin777@gmail.com>
2024-03-06 07:50:37 -0800

    frontend: useLogin hook update

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -13,6 +13,8 @@ export const useLogin = () => {
   const { dispatch } = UseAuthContext();
   const navigate = useNavigate();
 
+  console.log(import.meta.env.VITE_API_URL);
+
   const login = async (formBody) => {
     setLoading(true);
     setDisabled(true);
